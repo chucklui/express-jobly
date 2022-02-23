b3e6ded8ca0597cce8dfef4a96eb49aa2ba20b54
Chuck Lui <luialex0@gmail.com>
2022-02-23 12:18:50 -0800

    fixed the test for sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,21 +2,28 @@ const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 /**this function accepts an object of key-value pairs of data
- * and another object of {firstName: 'Aliya', age: 32}
+ * Example Inputs:
+ * dataToUpdate { lastName: 'chuck1' }
+ * jsToSql { firstName: 'first_name', lastName: 'last_name', isAdmin: 'is_admin' }
  * and returns {
- * setCols: "\"first_name\"=$1""\"age\"=$2",
- * values: [values of dataToUpdate,...]
+ * setCols: '"last_name"=$1',
+ * values: ["chuck1"]
  * }
  */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  console.log("dataToUpdate", dataToUpdate);
+  console.log("jsToSql", jsToSql);
+
+  //["lastName"]
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  // cols = ['"last_name"=$1'];
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
-
+  console.log("cols",cols);
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,15 +1,35 @@
 const { sqlForPartialUpdate } = require("./sql");
 
-// data to update { firstName, lastName, password, email }
+/**this test for valid inputs for sqlForPartialUpdate */
 describe("sqlForPartialUpdate", function() {
-    test("valid input", function() {
+    test("valid input: 1", function() {
         const result = sqlForPartialUpdate(
-            { firstName: "Yong" },
-            { firstName: "Chuck", lastName: "Lui"});
+            { lastName: "chuck" },
+            { firstName: 'first_name', lastName: 'last_name', isAdmin: 'is_admin' });
 
         expect(result).toEqual({
-            "setCols": "\"Chuck\"=$1", 
-            "values": ["Yong"]});
+            setCols: '"last_name"=$1',
+            values: ["chuck"]
+            });
     });
 
+    test("valid input: 2", function() {
+        const result = sqlForPartialUpdate(
+            { middle_name: "chuck" },
+            { firstName: 'first_name', lastName: 'last_name', isAdmin: 'is_admin' });
+
+        expect(result).toEqual({
+            setCols: '"middle_name"=$1',
+            values: ["chuck"]
+            });
+    });
+
+    // test("invalid input", function() {
+    //     const result = sqlForPartialUpdate(
+    //         {},
+    //         { firstName: 'first_name', lastName: 'last_name', isAdmin: 'is_admin' });
+
+    //     expect(result.statusCode).toEqual(400);
+    //     expect(result.body.error.message).toEqual("No data");
+    // });
 });
